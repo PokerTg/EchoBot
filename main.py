62806bb5972b32beeda089b3cd8bf743e4588384
PokerTg <PokerTg@email.com>
2016-09-17 11:20:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import telebot
+from telebot import types
+sys.setdefaultencoding("utf-8")
 
 bot = telebot.TeleBot("***TOKEN***")
 
 @bot.message_handler(commands=['start'])
-def 
+def start(m):
+  cid = m.chat.id
+  markup = types.InlineKeyboardButton()
+  markup.add(types.InlineKeyboardButton('English', callback_data='English'),types.InlineKeyboardButton("فارسی",callback_data='Persian'))
+  bot.send_message(cid, 'Choose your language:\n\nزبان خود را انتخاب کنید:'reply_markup=markup)
