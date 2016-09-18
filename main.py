15b286a9af61c9ba0c0500c5961133b3d0e4ab43
PokerTg <PokerTg@email.com>
2016-09-18 03:32:29 -0700

    Edit main,py (DONT FINISHED)

--- main.py
+++ main.py
@@ -9,4 +9,17 @@ def start(m):
   cid = m.chat.id
   markup = types.InlineKeyboardButton()
   markup.add(types.InlineKeyboardButton('English', callback_data='English'),types.InlineKeyboardButton("فارسی",callback_data='Persian'))
-  bot.send_message(cid, 'Choose your language:\n\nزبان خود را انتخاب کنید:'reply_markup=markup)
+  bot.send_message(cid, '***TEXT***',reply_markup=markup,parse_mode="Markdown")
+  
+@bot.callback_query_handler(func=lambda call:True)
+def callback_inline(call):
+  if call.message:
+    if call.data == 'English':
+      markupen = types.InlineKeyboardButton()
+      markupen.add(types.InlineKeyboardButton("Change language", callback_data="cl")
+      bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="***TEXT***"parse_mode="Markdown",reply_markup=markupen)
+  if call.message:
+    if call.data == 'cl':'
+    markupcl = types.InlineKeayboardButton()
+    markup.add(types.InlineKeyboardButton('English', callback_data='English'),types.InlineKeyboardButton("فارسی",callback_data='Persian'))
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="***TEXT***"parse_mode="Markdown",reply_markup=markupen)
